508374c5523719f380af2f85f3d7f3da1671121b
Elix1D <71213234+Elix1D@users.noreply.github.com>
2020-10-17 00:20:33 +0200

    Create LogRead.java

---
+++ LogRead.java
@@ -0,0 +1,9 @@
+package com.eli.cybersecurity.capstone;
+
+
+public class LogRead {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from " + LogRead.class.getSimpleName());
+    }
+}
